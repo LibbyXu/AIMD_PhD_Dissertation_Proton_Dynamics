18c4f9c52458b92e830ad08003e8588d387381d1
Libby Xu <xulihua66@hotmail.com>
2020-06-11 16:18:11 -0700

    Details Change

--- 1_Combine_XDATCAR_Pre_Analysis/Pre_Traj_check.sh
+++ 1_Combine_XDATCAR_Pre_Analysis/Pre_Traj_check.sh
@@ -1,37 +1,31 @@
-#This files will only work when you have more than 1 XDATCAR file
-#How to use this script
-#./connect_traj_outp.sh > LOG
+#Only works when you have more than 1 XDATCAR_# & vasp_# file
+#Typing the following command to conduct the data processing:./Pre_Traj_check.sh > LOG or sbatch Submit.run 
 
-#Connect different XDATCAR files
+#Connect & combine all "XDATCAR" files (in order)
 ls -l  *XDATCAR* > num_XDATCAR
 Num_X=`wc -l num_XDATCAR | cut -d' ' -f1`
-
 cp XDATCAR_1 XDATCAR_temp
+
 for ((i=2; i<=${Num_X}; i++))
-do0
-#without selective dynamics option
-sed -i '1,7d' XDATCAR_$i
-#with selective dynamics option
-#sed -i '1,8d' XDATCAR_$i
+do
+sed -i '1,7d' XDATCAR_$i #without selective dynamics option
+#sed -i '1,8d' XDATCAR_$i #with selective dynamics option
 cat XDATCAR_temp XDATCAR_$i > XDATCAR
 cp XDATCAR XDATCAR_temp 
 done
-
 rm XDATCAR_temp num_XDATCAR
 
 grep 'Direct' XDATCAR > numlines
 numline=`wc -l numlines | cut -d' ' -f1`
 rm numlines
-#The number of steps included
-echo ${numline}
+echo ${numline} #The number of steps included
 
-#check the total energy as well as the temperature for the AIMD simulations
-#same as the previous combining XDATCAR step, the script only apply to the vasp.out which have more than 2 files
+#Check the total energy & temperature variations along AIMD trajectories to obtain the relative stabilized simulation period.
 ls -l *vasp* > num_VASP
 Num_v=`wc -l num_VASP | cut -d' ' -f1`
 rm num_VASP
-
 cp vasp_1.out vasp_temp.out
+
 for ((i=2; i<=${Num_v}; i++))
 do
 cat vasp_temp.out vasp_$i.out > vasp.out
@@ -42,13 +36,8 @@ rm vasp_temp.out
 grep "T= " vasp.out | awk '{print $3" "$5" "$7}' > T_E_temp.dat
 cat -n T_E_temp.dat > T_E.dat
 rm T_E_temp.dat
-
 num_lines=`wc -l T_E.dat | cut -d' ' -f1`
 echo ${num_lines}
 
-# You need to make sure that the ${numline} == ${num_lines}. This means your code is right
-rm *vasp_*
-rm *XDATCAR_*
-
-
-
+# If ${numline} == ${num_lines}, your code is right. (check LOG file for the upper two rows)
+rm *vasp_* *XDATCAR_*
--- 1_Combine_XDATCAR_Pre_Analysis/Readme.md
+++ 1_Combine_XDATCAR_Pre_Analysis/Readme.md
@@ -1,35 +1,37 @@
 # Preliminary Treatment of Output Files
 
-Before we start to analyze any features for the ion/proton dynamic behaviors, we need to process the data combination & cleaning.
+Before we start to analyze different features for the ion/proton dynamic behaviors, we need to process the combination & cleaning of the high-volume data.
 
 ## Main Scripts & Functions
 
-**Submit.run** Linux Submmit Files
+**i)** **`Submit.run`**: Linux submmit file
 
-This script will try to connect the separated vasp.out and XDATCAR files togather. Then we will get the T_E.dat and the combined XDATCAR and vasp.out
+**ii)** **`Pre_Traj_check.sh`**: Code processing file
+
+This script aims to combine the separated **"vasp_#.out" & "XDATCAR_#"** files (due to the limited simulation time to perform **_Ab-initio_ molecular dynamic "AIMD"** each time). 
+
+After executing the commands, we will get the ouput files: **"T_E.dat"**, the combined **"XDATCAR" & "vasp.out"**. 
+
+## About Outputs
+
+**i)** **`T_E.dat`**: Contained the information: *step number, temperature, total free energy.*
+
+**ii)** **`XDATCAR`**: The combined whole trajectory file. 
+
+**iii)** **`vasp.out`**: The combined whole “OSZICAR” file.
 
 ## Processing Scripts
 
-we need the file:
+#### Files needed to be put in the same (current) folder before typing commands:
+
+Separated vasp.out files (more than 2 files): **`vasp_1.out, vasp_2.out, vasp_2.out, ……`**
+
+Separated trajectory file (more than 2 files): **`XDATCAR_1, XDATCAR_2, XDATCAR_3, ……`**
+
+Scripts & submit files: **`Pre_Traj_check.sh, Submit.run`**
 
 ****
 
-vasp_1.out
-
-vasp_2.out
-
-vasp_3.out
-
-.........
-
-XDATCAR_1
-
-XDATCAR_2
-
-XDATCAR_3
-
-.........
+**Typing the command**: **`sbatch Submit.run`** or **`./Pre_Traj_check.sh > LOG`**
 
 ****
-
-Then we need to do the command: sbatch Submit.run
